8d425b54793e9e88b51fc3286539e85b004a8fef
Hunor Laszlo Szasz <szasz12@gmail.com>
2024-02-20 15:23:42 +0200

    20-02-2024

--- src/routes/Tooltips/Tooltip.tsx
+++ src/routes/Tooltips/Tooltip.tsx
@@ -1,52 +1,8 @@
-import { useEffect, useState, useCallback } from 'react';
 import { useFetchData } from '@/utils/dataHelpers';
 import learnMoreSVG from '@assets/learnMoreIcon.svg';
 
-interface FetchedData {
-  data: Attributes;
-  videoData?: Blob;
-}
-
-interface Attributes {
-  title?: string;
-  createdAt?: string;
-  updatedAt?: string;
-  publishedAt?: string;
-  locale?: string;
-  description?: string;
-  shortcut?: string;
-  customID?: string;
-  extendedDescription?: string;
-  learnMore?: string;
-  videoData?: string;
-}
-
 function Tooltip() {
-  const [data, setData] = useState<Attributes | null>(null);
-  const [videoUrl, setVideoUrl] = useState<string | null>(null);
-
-  const fetchData = useCallback(useFetchData('tooltips', '1'), []);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const fetchedData = (await fetchData()) as FetchedData;
-        setData(fetchedData.data as Attributes); // Explicitly type fetchedData as Attributes
-
-        // Create a URL for the video blob
-        if (fetchedData.videoData) {
-          const url = URL.createObjectURL(fetchedData.videoData);
-          setVideoUrl(url);
-        } else {
-          setVideoUrl(null); // Clear the video URL if media.data is null
-        }
-      } catch (error) {
-        console.error('Failed to fetch data:', error);
-      }
-    };
-
-    getData();
-  }, [fetchData]);
+  const { data, videoUrl } = useFetchData('tooltips', '1');
 
   if (data === null) {
     return <div>Loading...</div>;
--- src/utils/dataHelpers.ts
+++ src/utils/dataHelpers.ts
@@ -1,43 +1,103 @@
 import axios from 'axios';
 import { useIndexedDB } from 'react-indexed-db-hook';
+import { useEffect, useState } from 'react';
 
-export const useFetchData = (endpoint: string, id: string) => {
-  const { getByID, add } = useIndexedDB('TooltipsObjectStore');
+interface FetchedData {
+  data: Attributes;
+  videoData?: Blob;
+}
 
-  const fetchData = async () => {
-    // Try to get the data from the cache
-    const cachedData = await getByID(id);
+interface Attributes {
+  title?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  publishedAt?: string;
+  locale?: string;
+  description?: string;
+  shortcut?: string;
+  customID?: string;
+  extendedDescription?: string;
+  learnMore?: string;
+  videoData?: string;
+}
 
-    if (cachedData) {
-      // If the data exists in the cache, return it
-      return cachedData;
-    } else {
-      // If the data doesn't exist in the cache, fetch it from the API
+export function useFetchData(endpoint: string, id: string) {
+  const { getByID, add, update } = useIndexedDB('TooltipsObjectStore');
+
+  const [data, setData] = useState<Attributes | null>(null);
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Try to get the data from the cache
+      const cachedData = await getByID(id);
       const tempAPIDomain = 'http://hu1w2021288.graphisoft.hu:1337';
-      const response = await axios.get(
-        `${tempAPIDomain}/api/${endpoint}/${id}?populate=media`
+      const cachedLastUpdated = cachedData?.data?.updatedAt;
+      const updatedData = await axios.get(
+        `${tempAPIDomain}/api/${endpoint}/${id}`
       );
-      const data = response.data.data.attributes;
+      const dataWasModified =
+        updatedData?.data?.data.attributes.updatedAt !== cachedLastUpdated;
 
-      let videoData = null;
-      // Check if media.data exists before trying to fetch the video data
-      if (data.media.data) {
-        const videoResponse = await axios.get(
-          tempAPIDomain + data.media.data.attributes.url,
-          {
-            responseType: 'blob',
-          }
+      const fetchDataFromAPI = async () => {
+        // If the data doesn't exist in the cache, fetch it from the API
+        const response = await axios.get(
+          `${tempAPIDomain}/api/${endpoint}/${id}?populate=media`
         );
-        videoData = await videoResponse.data;
+        const data = response.data.data.attributes;
+
+        let videoData = null;
+        // Check if media.data exists before trying to fetch the video data
+        if (data.media.data) {
+          const videoResponse = await axios.get(
+            tempAPIDomain + data.media.data.attributes.url,
+            {
+              responseType: 'blob',
+            }
+          );
+          videoData = await videoResponse.data;
+        }
+
+        // Store the fetched data and video data in IndexedDB for future use
+        if (cachedData && dataWasModified) {
+          await update({ id, data, videoData });
+        } else {
+          await add({ id, data, videoData });
+        }
+
+        // Return the fetched data and video data
+        return { data, videoData };
+      };
+
+      if (cachedData && !dataWasModified) {
+        // If the data exists in the cache, return it
+        return cachedData;
+      } else if (cachedData && dataWasModified) {
+        return await fetchDataFromAPI();
+      } else {
+        return await fetchDataFromAPI();
       }
+    };
 
-      // Store the fetched data and video data in IndexedDB for future use
-      await add({ id, data, videoData });
+    const getData = async () => {
+      try {
+        const fetchedData = (await fetchData()) as FetchedData;
+        setData(fetchedData.data as Attributes); // Explicitly type fetchedData as Attributes
 
-      // Return the fetched data and video data
-      return { data, videoData };
-    }
-  };
+        // Create a URL for the video blob
+        if (fetchedData.videoData) {
+          const url = URL.createObjectURL(fetchedData.videoData);
+          setVideoUrl(url);
+        } else {
+          setVideoUrl(null);
+        }
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+      }
+    };
 
-  return fetchData;
-};
+    getData();
+  }, [endpoint, id, getByID, add, update]);
+
+  return { data, videoUrl };
+}
